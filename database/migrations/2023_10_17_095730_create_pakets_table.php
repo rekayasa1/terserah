0f1f76321c74980e5fc823b19b421292d6a4547b
rekayasa1 <148739423+rekayasa1@users.noreply.github.com>
2023-10-23 11:22:08 +0700

    1

---
+++ database/migrations/2023_10_17_095730_create_pakets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Type\Integer;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pakets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('outlet_id')->constrained();
+            $table->enum('jenis',['kiloan','selimut','bed_cover',]);
+            $table->string('nama_paket');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pakets');
+    }
+};
